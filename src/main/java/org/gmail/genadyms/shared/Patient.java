551067f3b04031a429b6b34eae7b372ce4f9a4d4
Gennady <genadyms@gmail.com>
2017-01-23 15:18:43 +0300

    add AbstractModel

--- src/main/java/org/gmail/genadyms/shared/Patient.java
+++ src/main/java/org/gmail/genadyms/shared/Patient.java
@@ -1,21 +1,15 @@
 package org.gmail.genadyms.shared;
 
-import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "patient")
-public class Patient implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+public class Patient extends AbstractModel {
 
 	@Basic
 	@Column(name = "first_name")//, nullable = false, updatable = false, length = 150)
@@ -33,72 +27,25 @@ public class Patient implements Serializable {
     @Column(name = "created")
 	private Date created;
 
+	
+	
+//	@Id
+//	@Column(name = "id")
+////	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long id;
+
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+
 	@Override
 	public String toString() {
 		return "Patient [firstName=" + firstName + ", secondName=" + secondName + ", address=" + address + ", created="
-				+ created + ", id=" + id + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((address == null) ? 0 : address.hashCode());
-		result = prime * result + ((created == null) ? 0 : created.hashCode());
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((secondName == null) ? 0 : secondName.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Patient other = (Patient) obj;
-		if (address == null) {
-			if (other.address != null)
-				return false;
-		} else if (!address.equals(other.address))
-			return false;
-		if (created == null) {
-			if (other.created != null)
-				return false;
-		} else if (!created.equals(other.created))
-			return false;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (secondName == null) {
-			if (other.secondName != null)
-				return false;
-		} else if (!secondName.equals(other.secondName))
-			return false;
-		return true;
-	}
-
-	@Id
-	@Column(name = "id")
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
+				+ created + "]";
 	}
 
 	public String getFirstName() {
