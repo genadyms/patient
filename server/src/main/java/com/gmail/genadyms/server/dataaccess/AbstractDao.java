238989355adc164efa5d3e012628b6a4c8c6a3e2
genadyms <genadyms@gmail.com>
2017-02-10 13:55:56 +0300

    delete comments

--- server/src/main/java/com/gmail/genadyms/server/dataaccess/AbstractDao.java
+++ server/src/main/java/com/gmail/genadyms/server/dataaccess/AbstractDao.java
@@ -9,59 +9,55 @@ import com.gmail.genadyms.server.dataaccess.util.EntityManagerFactoryBuilder;
 
 public class AbstractDao<T, ID> {
 
-	private EntityManager entityManager;
+    private EntityManager entityManager;
 
-	private final Class<T> entityClass;
+    private final Class<T> entityClass;
 
-	protected AbstractDao(final Class<T> entityClass) {
-		this.entityManager = EntityManagerFactoryBuilder.getEntityManagerFactory().createEntityManager();
-		this.entityClass = entityClass;
-		System.out.println("--------------------"+this.getClass().getName()+"---------------------------");
-	}
+    protected AbstractDao(final Class<T> entityClass) {
+        this.entityManager = EntityManagerFactoryBuilder.getEntityManagerFactory().createEntityManager();
+        this.entityClass = entityClass;
+        System.out.println("--------------------" + this.getClass().getName() + "---------------------------");
+    }
 
-	public List<T> getAll() {
-		final CriteriaQuery<T> query = entityManager.getCriteriaBuilder().createQuery(getEntityClass());
-		query.from(getEntityClass());
-		final List<T> lst = entityManager.createQuery(query).getResultList();
-		return lst;
-	}
+    public List<T> getAll() {
+        final CriteriaQuery<T> query = entityManager.getCriteriaBuilder().createQuery(getEntityClass());
+        query.from(getEntityClass());
+        final List<T> lst = entityManager.createQuery(query).getResultList();
+        return lst;
+    }
 
-	public T get(final ID id) {
-		return entityManager.find(getEntityClass(), id);
-	}
+    public T get(final ID id) {
+        return entityManager.find(getEntityClass(), id);
+    }
 
-	public T insert(final T entity) {
-		getEntityManager().getTransaction().begin();
-		entityManager.persist(entity);
-		getEntityManager().getTransaction().commit();
-		return entity;
-	}
+    public T insert(final T entity) {
+        getEntityManager().getTransaction().begin();
+        entityManager.persist(entity);
+        getEntityManager().getTransaction().commit();
+        return entity;
+    }
 
-	public T update(T entity) {
-		getEntityManager().getTransaction().begin();
-		entity = entityManager.merge(entity);
-		getEntityManager().getTransaction().commit();
-		return entity;
-	}
+    public T update(T entity) {
+        getEntityManager().getTransaction().begin();
+        entity = entityManager.merge(entity);
+        getEntityManager().getTransaction().commit();
+        return entity;
+    }
 
-	public void delete(T entity) {
-		getEntityManager().getTransaction().begin();
-		getEntityManager().remove(entity);
-		getEntityManager().getTransaction().commit();
-	}
+    public void delete(T entity) {
+        getEntityManager().getTransaction().begin();
+        getEntityManager().remove(entity);
+        getEntityManager().getTransaction().commit();
+    }
 
-	public Class<T> getEntityClass() {
-		return entityClass;
-	}
+    public Class<T> getEntityClass() {
+        return entityClass;
+    }
 
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
-	/*
-	 * protected void setPaging(AbstractFilter filter, TypedQuery<? extends
-	 * AbstractModel> q) { if (filter.getOffset() != null && filter.getLimit()
-	 * != null) { q.setFirstResult(filter.getOffset());
-	 * q.setMaxResults(filter.getLimit()); } }
-	 */
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+
 
 }
