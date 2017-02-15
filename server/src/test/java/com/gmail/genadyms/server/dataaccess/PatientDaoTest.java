3d4abe5d71e2d1e21092d05e622a28951ee38c6f
genadyms <genadyms@gmail.com>
2017-02-15 23:48:22 +0300

    refactor tests

--- server/src/test/java/com/gmail/genadyms/server/dataaccess/PatientDaoTest.java
+++ server/src/test/java/com/gmail/genadyms/server/dataaccess/PatientDaoTest.java
@@ -2,73 +2,68 @@ package com.gmail.genadyms.server.dataaccess;
 
 import org.junit.Test;
 
-import com.gmail.genadyms.server.dataaccess.PatientDao;
 import com.gmail.genadyms.server.datamodel.Ward;
 import com.gmail.genadyms.server.datamodel.Patient;
 
 import java.util.Date;
-import java.util.List;
 
 import org.junit.Assert;
 
-public class PatientDaoTest extends WardDaoTest{
-	private PatientDao dao = new PatientDao();
-	
-	@Test
-	public void testSave() {
-		Patient patient = saveMockPatient();
-		Assert.assertNotNull(patient.getId());
-		clearDb(patient);
-	}
+public class PatientDaoTest extends WardDaoTest {
+    private PatientDao dao = new PatientDao();
 
-	private void clearDb(Patient patient) {
-		Ward bp = patient.getWard();	
-		dao.delete(patient);
-		clearBedPlace(bp);
-	}
+    @Test
+    public void testSave() {
+        Patient patient = saveMockPatient();
+        Assert.assertNotNull(patient.getId());
+        clearDb(patient);
+    }
 
-	@Test
-	public void testUpdate() {
-		Patient patient = saveMockPatient();
-		String changeFirstName = "change-firstname";
-		patient.setFirstName(changeFirstName);
-		dao.update(patient);
-		Assert.assertEquals(changeFirstName, patient.getFirstName());
-		clearDb(patient);
-	}
+    private void clearDb(Patient patient) {
+        Ward bp = patient.getWard();
+        dao.delete(patient);
+        clearMockWard(bp);
+    }
 
-//	@Test
-//	public void testChangeAll(){
-//		List<Patient> allList1 = dao.getAll();
-//		System.out.println(allList1.size());
-//		Patient p = null;
-//		for (Patient cur : allList1) {
-//			if(null==p) {
-//				p=cur;
-//			}
-//			System.out.println(cur.getAddress());
-//		}
-//		p.setAddress("get all example");
-//		System.out.println("----------------------------------------------");
-//		dao.update(p);
-//		List<Patient> allList2 = dao.getAll();
-//		System.out.println(allList2.size());
-//		for (Patient cur : allList2) {
-//			System.out.println(cur.getAddress());
-//		}
-//		
-//	}
-	
-	private Patient saveMockPatient() {
-		Patient p = new Patient();
-		p.setFirstName("first-name-" + String.valueOf(Math.random()));
-		p.setLastName("last-name-" + String.valueOf(Math.random()));
-		p.setAddress(("home-address-" + String.valueOf(Math.random())));
-		p.setDiagnosis("diagnosis");
-		p.setComingDate(new Date());
-		Ward bedPlace = saveMockBedPlace(33, 2);
-		p.setWard(bedPlace);
-		dao.insert(p);
-		return p;
-	}
+    @Test
+    public void testUpdate() {
+        Patient patient = saveMockPatient();
+        String changeFirstName = "change-firstname";
+        patient.setFirstName(changeFirstName);
+        dao.update(patient);
+        Assert.assertEquals(changeFirstName, patient.getFirstName());
+        clearDb(patient);
+    }
+
+    @Test
+    public void testUpdateWard() {
+        Patient patient = saveMockPatient();
+        Ward oldWard = patient.getWard();
+        Ward ward = saveMockWard(34, 25);
+        Assert.assertNotNull(ward);
+        Assert.assertNotEquals(ward.getId(), patient.getWard().getId());
+        patient.setWard(ward);
+        dao.update(patient);
+        Assert.assertEquals(ward.getId(), patient.getWard().getId());
+        clearDb(patient);
+        clearMockWard(oldWard);
+    }
+
+    private Patient saveMockPatient(Integer numberWard, Integer countBeds) {
+        Patient p = new Patient();
+        p.setFirstName("first-name-" + String.valueOf(Math.random()));
+        p.setLastName("last-name-" + String.valueOf(Math.random()));
+        p.setAddress(("home-address-" + String.valueOf(Math.random())));
+        p.setDiagnosis("diagnosis");
+        p.setComingDate(new Date());
+        Ward bedPlace = saveMockWard(numberWard, countBeds);
+        p.setWard(bedPlace);
+        dao.insert(p);
+        return p;
+    }
+
+    private Patient saveMockPatient() {
+        return saveMockPatient(33, 5);
+    }
+
 }
--- server/src/test/java/com/gmail/genadyms/server/dataaccess/WardDaoTest.java
+++ server/src/test/java/com/gmail/genadyms/server/dataaccess/WardDaoTest.java
@@ -11,7 +11,7 @@ public class WardDaoTest {
 	@Test
 	public void testSave() {
 		Long totalStart = dao.count();
-		Ward bedPlace = saveMockBedPlace(21, 21);
+		Ward bedPlace = saveMockWard(21, 21);
 		Long totalSave = dao.count()-1;
 		Assert.assertEquals(totalStart, totalSave);
 		Assert.assertNotNull(bedPlace.getId());
@@ -20,13 +20,13 @@ public class WardDaoTest {
 	
 	@Test
 	public void testDelete() {
-		Ward bedPlace = saveMockBedPlace(22,22);
+		Ward bedPlace = saveMockWard(22,22);
 		dao.delete(bedPlace);
 		Ward delPlace = dao.get(bedPlace.getId());
 		Assert.assertNull(delPlace);
 	}
 	
-	protected Ward saveMockBedPlace(Integer nChamber, Integer nBed) {
+	protected Ward saveMockWard(Integer nChamber, Integer nBed) {
 		Ward mock = new Ward();
 		mock.setNumber(nChamber);
 		mock.setCountBeds(nBed);
@@ -34,7 +34,7 @@ public class WardDaoTest {
 		return mock;
 	}
 	
-	protected void clearBedPlace(Ward ward) {
+	protected void clearMockWard(Ward ward) {
 		dao.delete(ward);
 	}
 }
