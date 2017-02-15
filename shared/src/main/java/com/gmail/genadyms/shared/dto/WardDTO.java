cef585d1f180fa2c5df0e254c946485e7bd2391b
genadyms <genadyms@gmail.com>
2017-02-15 14:13:46 +0300

    remove field id

--- shared/src/main/java/com/gmail/genadyms/shared/dto/WardDTO.java
+++ shared/src/main/java/com/gmail/genadyms/shared/dto/WardDTO.java
@@ -5,18 +5,9 @@ import java.io.Serializable;
 public class WardDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private Long id;
-    private Integer numberWard;
+     private Integer numberWard;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNumberWard(Integer numberWard) {
+        public void setNumberWard(Integer numberWard) {
         this.numberWard = numberWard;
     }
 
@@ -26,9 +17,6 @@ public class WardDTO implements Serializable {
 
     @Override
     public String toString() {
-        return "WardDTO{" +
-                "id=" + id +
-                ", numberWard=" + numberWard +
-                '}';
+        return "WardDTO{" + ", numberWard=" + numberWard + '}';
     }
 }
