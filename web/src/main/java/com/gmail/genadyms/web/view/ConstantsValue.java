916d90b9103470c4d7be52eede90d236d0d7baaf
genadyms <genadyms@gmail.com>
2017-02-16 12:56:26 +0300

    move some Strings to enum class

--- web/src/main/java/com/gmail/genadyms/web/view/ConstantsValue.java
+++ web/src/main/java/com/gmail/genadyms/web/view/ConstantsValue.java
@@ -1,7 +1,29 @@
 package com.gmail.genadyms.web.view;
 
-/**
- * Created by mgm on 16.2.17.
- */
+
 public enum ConstantsValue {
+    FIRST_NAME("First name"),
+    LAST_NAME("Last name"),
+    DIAGNOSIS("Diagnosis"),
+    ADDRESS("Home address"),
+    COMING_DATE("Coming"),
+    LEAVING_DATE("Leaving"),
+    WARD("Ward"),
+    FORMAT_TEMPLATE("MM/dd/yyyy"),
+    BUTTON_ADD("Add patient"),
+    BUTTON_SAVE("Save patient "),
+    BUTTON_CANCEL("Cancel"),
+    BUTTON_UPDATE("Update patient"),
+    TABLE_TITLE("table of patients");
+
+    private final String value;
+
+    private ConstantsValue(final String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
