02d8c03273feaf4c6106376ac4a41ec9d507c178
genadyms <genadyms@gmail.com>
2017-02-16 17:23:43 +0300

    add getFreeWards

--- server/src/main/java/com/gmail/genadyms/server/dataaccess/WardDao.java
+++ server/src/main/java/com/gmail/genadyms/server/dataaccess/WardDao.java
@@ -2,11 +2,10 @@ package com.gmail.genadyms.server.dataaccess;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 
 import com.gmail.genadyms.server.dataaccess.filter.BedPlaceFilter;
+import com.gmail.genadyms.server.datamodel.Patient;
 import com.gmail.genadyms.server.datamodel.Ward;
 
 import java.util.List;
@@ -44,5 +43,18 @@ public class WardDao extends AbstractDao<Ward, Long> {
 		} else {
 			throw new IllegalArgumentException("more than 1 ward found ");
 		}
+	}
+
+	public List<Ward> getFreeWard() {
+		EntityManager em = getEntityManager();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Ward> cq = cb.createQuery(Ward.class);
+		Root<Ward> from = cq.from(Ward.class);
+//		cq.where(cb.isNull(from.get("patients").get("leavingDate")));
+//		Ward_
+//		Predicate pred = cb.ge(from.get(Ward_.), y)
+//		cq.where()
+		TypedQuery<Ward> q = em.createQuery(cq);
+		return q.getResultList();
 	}
 }
--- server/src/test/java/com/gmail/genadyms/server/dataaccess/WardDaoTest.java
+++ server/src/test/java/com/gmail/genadyms/server/dataaccess/WardDaoTest.java
@@ -1,31 +1,44 @@
 package com.gmail.genadyms.server.dataaccess;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.gmail.genadyms.server.datamodel.Ward;
 
+import java.util.List;
+
 public class WardDaoTest {
 	private WardDao dao = new WardDao();
-	
+
 	@Test
 	public void testSave() {
 		Long totalStart = dao.count();
 		Ward bedPlace = saveMockWard(21, 21);
-		Long totalSave = dao.count()-1;
+		Long totalSave = dao.count() - 1;
 		Assert.assertEquals(totalStart, totalSave);
 		Assert.assertNotNull(bedPlace.getId());
 		dao.delete(bedPlace);
 	}
-	
+
 	@Test
 	public void testDelete() {
-		Ward bedPlace = saveMockWard(22,22);
+		Ward bedPlace = saveMockWard(22, 22);
 		dao.delete(bedPlace);
 		Ward delPlace = dao.get(bedPlace.getId());
 		Assert.assertNull(delPlace);
 	}
-	
+
+	@Ignore
+	@Test
+	public void testGetFreeWards() {
+		List<Ward> wards = dao.getFreeWard();
+		Assert.assertNotNull(wards.size());
+		for (Ward ward : wards) {
+			System.out.println(ward.getNumber());
+		}
+	}
+
 	protected Ward saveMockWard(Integer nChamber, Integer nBed) {
 		Ward mock = new Ward();
 		mock.setNumber(nChamber);
@@ -33,7 +46,7 @@ public class WardDaoTest {
 		dao.insert(mock);
 		return mock;
 	}
-	
+
 	protected void clearMockWard(Ward ward) {
 		dao.delete(ward);
 	}
