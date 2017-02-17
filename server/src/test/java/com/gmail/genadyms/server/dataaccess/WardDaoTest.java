c404caf8faa8c4ae22a42ff88ea55e4d006affc6
genadyms <genadyms@gmail.com>
2017-02-17 13:08:44 +0300

    new getFreeWards

--- server/src/test/java/com/gmail/genadyms/server/dataaccess/WardDaoTest.java
+++ server/src/test/java/com/gmail/genadyms/server/dataaccess/WardDaoTest.java
@@ -4,49 +4,75 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.gmail.genadyms.server.datamodel.Patient;
 import com.gmail.genadyms.server.datamodel.Ward;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class WardDaoTest {
-	private WardDao dao = new WardDao();
+	private WardDao daoWard = new WardDao();
+	private PatientDao daoPatient = new PatientDao();
 
 	@Test
 	public void testSave() {
-		Long totalStart = dao.count();
+		Long totalStart = daoWard.count();
 		Ward bedPlace = saveMockWard(21, 21);
-		Long totalSave = dao.count() - 1;
+		Long totalSave = daoWard.count() - 1;
 		Assert.assertEquals(totalStart, totalSave);
 		Assert.assertNotNull(bedPlace.getId());
-		dao.delete(bedPlace);
+		daoWard.delete(bedPlace);
 	}
 
 	@Test
 	public void testDelete() {
 		Ward bedPlace = saveMockWard(22, 22);
-		dao.delete(bedPlace);
-		Ward delPlace = dao.get(bedPlace.getId());
+		daoWard.delete(bedPlace);
+		Ward delPlace = daoWard.get(bedPlace.getId());
 		Assert.assertNull(delPlace);
 	}
 
+	@Ignore
 	@Test
 	public void testGetFreeWards() {
-		List<Ward> wards = dao.getFreeWard();
-		Assert.assertNotNull(wards.size());
-		for (Ward ward : wards) {
-			System.out.println(ward.getNumber());
+		List<Patient> patientsActive = daoPatient.findActivePatients();
+		Assert.assertNotNull(patientsActive);
+		Map<Ward, Integer> wardsPatients = new HashMap();
+		System.out.println("Total patients active is " + patientsActive.size());
+		for (Patient patient : patientsActive) {
+			Ward currentWard = patient.getWard();
+			if (wardsPatients.containsKey(currentWard)) {
+				Integer changeCountsPatients = wardsPatients.get(currentWard) + 1;
+				wardsPatients.put(currentWard, changeCountsPatients);
+				System.out.println(currentWard + "|" + changeCountsPatients + "|" + patient.getId());
+			} else {
+				wardsPatients.put(currentWard, 1);
+				System.out.println(currentWard + "|" + patient.getId());
+			}
 		}
+		Set<Ward> wardsFree = new HashSet();
+		for (Ward ward : wardsPatients.keySet()) {
+			System.out.println(ward.getCountBeds() + "|" + wardsPatients.get(ward));
+			if (wardsPatients.get(ward) < ward.getCountBeds()) {
+				wardsFree.add(ward);
+				System.out.println(ward);
+			}
+		}
+		Assert.assertNotNull(wardsFree);
 	}
 
 	protected Ward saveMockWard(Integer nChamber, Integer nBed) {
 		Ward mock = new Ward();
 		mock.setNumber(nChamber);
 		mock.setCountBeds(nBed);
-		dao.insert(mock);
+		daoWard.insert(mock);
 		return mock;
 	}
 
 	protected void clearMockWard(Ward ward) {
-		dao.delete(ward);
+		daoWard.delete(ward);
 	}
 }
