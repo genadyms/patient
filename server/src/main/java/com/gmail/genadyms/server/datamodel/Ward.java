bfd67e49e794754908ae59ce115c666dbe38b59c
genadyms <genadyms@gmail.com>
2017-02-17 13:07:07 +0300

    add equals

--- server/src/main/java/com/gmail/genadyms/server/datamodel/Ward.java
+++ server/src/main/java/com/gmail/genadyms/server/datamodel/Ward.java
@@ -18,6 +18,37 @@ public class Ward extends AbstractModel {
 	@Column
 	private Integer countBeds;
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((countBeds == null) ? 0 : countBeds.hashCode());
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ward other = (Ward) obj;
+		if (countBeds == null) {
+			if (other.countBeds != null)
+				return false;
+		} else if (!countBeds.equals(other.countBeds))
+			return false;
+		if (number == null) {
+			if (other.number != null)
+				return false;
+		} else if (!number.equals(other.number))
+			return false;
+		return true;
+	}
+
 	@OneToMany(mappedBy = "ward", fetch = FetchType.LAZY)
 	private List<Patient> patients;
 
